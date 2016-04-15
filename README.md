fd9f5d628f3c5e2c4f6c1ddc20c0c54c2ef15635
Gabriel de Maeztu <gabriel.maeztu@gmail.com>
2016-04-15 13:22:34 +0200

    Added docs and pic

--- README.md
+++ README.md
@@ -5,6 +5,17 @@ This is a tool to visualize graph database queries from Neo4j in the Jupyter Not
 #### Install
 `pip install neo4jupyter`
 
+<img width="921" alt="screen shot 2016-04-15 at 13 13 33" src="https://cloud.githubusercontent.com/assets/1485056/14559854/e2bca15a-030b-11e6-9b8f-9858143e4e40.png">
+
+
+#### Docs
+
+```python
+import neo4jupyter
+neo4jupyter.init_notebook_mode()
+neo4jupyter.draw(graph_object_py2neo, {"Nodes_type": "Att", …})
+```
+
 #### Licence
 The MIT License (MIT) | See LICENSE.md
 Copyright (c) 2015, 2016 Gabi de Maeztu
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='neo4jupyter',
     version='0.1.0',
+    keywords='jupyter neo4j graphdb',
     url='https://github.com/merqurio/neo4jupyter',
     license='MIT',
     author='Gabriel de Maeztu',
@@ -14,5 +15,14 @@ setup(
     install_requires=[
         'IPython >= 4.0.0',
         'py2neo'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
     ]
 )
